41c33f306ad8072c9a970537288080f0c3ae7f3e
Kian <kianbehjati_84@outlook.com>
2026-07-10 12:22:56 +0330

    New Transfer UI + forms fix
        - replaced class with form-control for more modern input
    
        - modern UI

--- card/forms.py
+++ card/forms.py
@@ -3,16 +3,16 @@ from django import forms
 class Transfer(forms.Form):
 
     to = forms.CharField(max_length=36,min_length=36,widget=forms.TextInput(attrs={
-        'class':"input",
+        'class':"form-control",
         "placeholder":"destination Card",
 
     }))
     amount = forms.IntegerField(widget=forms.NumberInput(attrs={
-        'class':"input",
+        'class':"form-control",
         "placeholder":"amount",
 
     }))
     description = forms.CharField(required=False,widget=forms.Textarea(attrs={
-        'class':"description",
+        'class':"description form-control",
     }))
 
