0b9fe4a921f9228a1167e79f6760782bb52e5426
Kian <kianbehjati_84@outlook.com>
2026-07-10 12:21:17 +0330

    New ContactUs UI + forms fix
        - replaced class with form-control for more modern input
    
        - modern UI

--- contactus/forms.py
+++ contactus/forms.py
@@ -2,15 +2,15 @@ from django import forms
 
 class Contact_form(forms.Form):
     title = forms.CharField(max_length=50,widget=forms.TextInput(attrs={
-        "class":"input",
+        "class":"form-control",
         "placeholder":"title"
     }))
     text = forms.CharField(widget=forms.Textarea(attrs={
-        "class":"text",
+        "class":"form-control",
         "placeholder": "details"
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
-        "class":"input",
+        "class":"form-control",
         "placeholder": "email"
     }))
 
